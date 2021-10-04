ead14f114bba0064bd5b6c61b9cbbe89435845a6
ZNBrown <ZachNatBrown@gmail.com>
2021-10-04 10:12:36 +0100

    adds working script and blank html page

---
+++ client/static/JS/index.js
@@ -0,0 +1,6 @@
+function exampleFetch() {
+    fetch("http://localhost:3000/")
+    .then(res=>res.json()).then(data => (console.log(data)))
+}
+
+exampleFetch();
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors');
 app.use(cors());
 const fetch = require('node-fetch');
+const journals = require('./journals.json')
 
 app.get('/', (req, res) => {
     res.json("hello world")

d2df492a37cd2d4030ba1df605bf7d1cb4e1a0da
ZNBrown <ZachNatBrown@gmail.com>
2021-10-07 19:12:34 +0100

    changes index

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const app = require('./server');
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log("we are listening") );
+app.listen(port, () => console.log(`we are listening port ${process.env.PORT}` ));
